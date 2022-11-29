b42dd925d7bdef5540e4b873dfbc80b0b66ce52f
JuliaKaz <106685861+JuliaKaz@users.noreply.github.com>
2022-11-29 11:37:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -111,41 +111,44 @@ def search_info(user_id):
   res = requests.get(url, params=params)
 
   json_res = res.json()
-  if 'bdate' in json_res['response'][0].keys() and \
+  try:
+    if 'bdate' in json_res['response'][0].keys() and \
           len(json_res['response'][0]['bdate']) > 7:
-    age_bot_user = int(json_res['response'][0]['bdate'][-4:])
-    age_to = (int(datetime.now().year) - age_bot_user) + 3
-    age_at = (int(datetime.now().year) - age_bot_user) - 3
+      age_bot_user = int(json_res['response'][0]['bdate'][-4:])
+      age_to = (int(datetime.now().year) - age_bot_user) + 3
+      age_at = (int(datetime.now().year) - age_bot_user) - 3
 
-  else:
-    write_msg(user_id, 'Возраст от:')
-    msg_text, user_id = loop_bot()
-    age_to = msg_text[0:1]
-    write_msg(user_id, 'Возраст до:')
-    msg_text, user_id = loop_bot()
-    age_at = msg_text[0:1]
+    else:
+      write_msg(user_id, 'Возраст от:')
+      msg_text, user_id = loop_bot()
+      age_to = msg_text[0:1]
+      write_msg(user_id, 'Возраст до:')
+      msg_text, user_id = loop_bot()
+      age_at = msg_text[0:1]
   # print(json_res)
 
-  sex_user = json_res['response'][0]['sex']
-  if sex_user == 1:
-    sex = 2
-  elif sex_user == 2:
-    sex = 1
+    sex_user = json_res['response'][0]['sex']
+    if sex_user == 1:
+      sex = 2
+    elif sex_user == 2:
+      sex = 1
 
-  else:
-    write_msg(user_id, 'Введите пол.\n1 - девушка\n2 - парень')
-    msg_text, user_id = loop_bot()
-    sex = msg_text[0:1]
+    else:
+      write_msg(user_id, 'Введите пол.\n1 - девушка\n2 - парень')
+      msg_text, user_id = loop_bot()
+      sex = msg_text[0:1]
 
-  if 'city' in json_res['response'][0]:
-    city = json_res['response'][0]['city']['title']
-    
-  else:
-    write_msg(user_id, 'Введите город')
-    msg_text, user_id = loop_bot()
-    city = msg_text[0:len(msg_text)].lower()
+    if 'city' in json_res['response'][0]:
+      city = json_res['response'][0]['city']['title']
 
-  return sex, age_to, age_at, city
+    else:
+      write_msg(user_id, 'Введите город')
+      msg_text, user_id = loop_bot()
+      city = msg_text[0:len(msg_text)].lower()
+
+    return sex, age_to, age_at, city
+  except KeyError:
+    write_msg(user_id, 'Ошибка получения токена')
 
 
 
@@ -153,7 +156,7 @@ if __name__ == '__main__':
   while True:
     msg_text, user_id = loop_bot()
 
-    if msg_text [0:7].lower() == 'vkinder':
+    if msg_text[0:7].lower() == 'vkinder':
       menu_bot(user_id)
       msg_text, user_id = loop_bot()
 
@@ -164,8 +167,8 @@ if __name__ == '__main__':
       # elif len(msg_text) > 1:
       #   sex = 0
       elif msg_text[0:3].lower() == 'ищи':
-
-        sex, age_to, age_at, city = search_info(user_id)
+        try:
+          sex, age_to, age_at, city = search_info(user_id)
 
         # if msg_text[0:7].lower() == 'девушка':
         #   sex = 1
@@ -182,77 +185,79 @@ if __name__ == '__main__':
         # city = msg_text[14:len(msg_text)].lower()
         # Ищем анкеты
 
-
-        result = search_users(sex, int(age_at), int(age_to), city)
-        json_create(result)
-        current_user_id = check_db_master(user_id)
-        # Отбирает анкеты
-        for i in range(len(result)):
-          dating_user, blocked_user = check_db_user(result[i][3])
-          # Получает фото и сортирует по лайкам
-          user_photo = get_photo(result[i][3])
-          if user_photo == 'нет доступа к фото' or dating_user is not None or blocked_user is not None:
-            continue
-          sorted_user_photo = sort_likes(user_photo)
-          # Выводит отсортированные данные по анкетам
-          write_msg(
-            user_id,
-            f'\n{result[i][0]}  {result[i][1]}  {result[i][2]}',
-          )
-          try:
-            write_msg(user_id,
-                      f'фото:',
-                      attachment=','.join([
-                        sorted_user_photo[-1][1], sorted_user_photo[-2][1],
-                        sorted_user_photo[-3][1]
-                      ]))
-          except IndexError:
-            for photo in range(len(sorted_user_photo)):
-              write_msg(user_id,
-                        f'фото:',
-                        attachment=sorted_user_photo[photo][1])
-          # Ждет ввод
-          write_msg(
-            user_id,
-            '1 - Добавить, 2 - Заблокировать, 0 - Далее, \nq - выход из поиска'
-          )
-          msg_text, user_id = loop_bot()
-          if msg_text == '0':
-            # Проверка на последнюю запись
-            if i >= len(result) - 1:
-              show_info()
-          # Добавляет пользователя в избранное
-          elif msg_text == '1':
-            # Проверка на последнюю запись
-            if i >= len(result) - 1:
-              show_info()
-              break
-            # Добавляет анкету в БД
+          result = search_users(sex, int(age_at), int(age_to), city)
+          json_create(result)
+          current_user_id = check_db_master(user_id)
+          # Отбирает анкеты
+          for i in range(len(result)):
+            dating_user, blocked_user = check_db_user(result[i][3])
+            # Получает фото и сортирует по лайкам
+            user_photo = get_photo(result[i][3])
+            if user_photo == 'нет доступа к фото' or dating_user is not None or blocked_user is not None:
+              continue
+            sorted_user_photo = sort_likes(user_photo)
+            # Выводит отсортированные данные по анкетам
+            write_msg(
+              user_id,
+              f'\n{result[i][0]}  {result[i][1]}  {result[i][2]}',
+            )
             try:
-              add_user(user_id, result[i][3], result[i][1], result[i][0], city,
-                       result[i][2], current_user_id.id)
-              # Добавляет фото в БД
-              add_user_photos(user_id, sorted_user_photo[0][1],
-                              sorted_user_photo[0][0], current_user_id.id)
-            except AttributeError:
-              write_msg(
-                user_id,
-                'Вы не зарегистрировались!\n Введите Vkinder для перезагрузки бота'
-              )
+              write_msg(user_id,
+                          f'фото:',
+                          attachment=','.join([
+                            sorted_user_photo[-1][1], sorted_user_photo[-2][1],
+                            sorted_user_photo[-3][1]
+                          ]))
+            except IndexError:
+              for photo in range(len(sorted_user_photo)):
+                write_msg(user_id,
+                            f'фото:',
+                            attachment=sorted_user_photo[photo][1])
+            # Ждет ввод
+            write_msg(
+              user_id,
+              '1 - Добавить, 2 - Заблокировать, 0 - Далее, \nq - выход из поиска'
+            )
+            msg_text, user_id = loop_bot()
+            if msg_text == '0':
+              # Проверка на последнюю запись
+              if i >= len(result) - 1:
+                show_info()
+            # Добавляет пользователя в избранное
+            elif msg_text == '1':
+              # Проверка на последнюю запись
+              if i >= len(result) - 1:
+                show_info()
+                break
+              # Добавляет анкету в БД
+              try:
+                add_user(user_id, result[i][3], result[i][1], result[i][0], city,
+                           result[i][2], current_user_id.id)
+                # Добавляет фото в БД
+                add_user_photos(user_id, sorted_user_photo[0][1],
+                                  sorted_user_photo[0][0], current_user_id.id)
+              except AttributeError:
+                write_msg(
+                  user_id,
+                  'Вы не зарегистрировались!\n Введите Vkinder для перезагрузки бота'
+                )
+                break
+            # Добавляет пользователя в черный список
+            elif msg_text == '2':
+              # Проверка на последнюю запись
+              if i >= len(result) - 1:
+                show_info()
+              # Блокирует
+              add_to_black_list(user_id, result[i][3], result[i][1],
+                                  result[i][0], city, result[i][2],
+                                  sorted_user_photo[0][1], sorted_user_photo[0][0],
+                                  current_user_id.id)
+            elif msg_text.lower() == 'q':
+              write_msg(user_id, 'До встречи.')
               break
-          # Добавляет пользователя в черный список
-          elif msg_text == '2':
-            # Проверка на последнюю запись
-            if i >= len(result) - 1:
-              show_info()
-            # Блокирует
-            add_to_black_list(user_id, result[i][3], result[i][1],
-                              result[i][0], city, result[i][2],
-                              sorted_user_photo[0][1], sorted_user_photo[0][0],
-                              current_user_id.id)
-          elif msg_text.lower() == 'q':
-            write_msg(user_id, 'До встречи.')
-            break
+        except Exception:
+          write_msg(user_id, 'Что-то пошло не так'
+                    '\nVkinder - для активации бота.')
 
       # Переходит в избранное
       elif msg_text == '2':
@@ -265,3 +270,5 @@ if __name__ == '__main__':
     elif len(msg_text) > 0:
       write_msg(user_id, f'Здравствуйте! '
                          f'\nВведите Vkinder для активации бота.')
+
+
